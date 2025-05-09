cdc936b3a0936e3ea8be519427ac1b9ed6e1fc5c
Eladio Romani <60736435@certus.edu.pe>
2025-05-09 09:44:51 -0500

    cambios en env

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(express.json());
 app.use(cors());
 
 
-const MONGO_URI = process.MONGO_URI;
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI).then(() => {
   console.log("Se conectó exitosamente ");
